a7482bcd6fc69043cb28e3393c0417940f3aad29
generaliru <ucamcahoa1600@alumno.ipn.mx>
2021-06-14 17:43:58 -0500

    DOM iniciando

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/* var parrafo = document.getElementsByClassName('parrafo');
+console.log(parrafo[0]) */
+/* document.querySelectorAll('.parrafo').textContent = 'Nuevo texto de parrafo'; */
+var parrafo1 = document.querySelector('.parrafo');
+var parrafo2 = document.querySelector('#parrafo2');
+
+console.log(document.head);
+console.log(document.body);
+console.log(parrafo1)
+console.log(parrafo2)
+
